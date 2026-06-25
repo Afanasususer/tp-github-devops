0f39ffa48910ea778bfe394a53adbd809743acca
Samadi Abdelouahab <samadiabdelouahab4@gmail.com>
2026-06-25 18:34:37 +0100

    feat: add delete user function

--- src/api.py
+++ src/api.py
@@ -6,3 +6,8 @@ def ajouter_utilisateur(nom, email):
 
 def chercher_utilisateur(email):
     return next((u for u in utilisateurs if u["email"] == email), None)
+
+def supprimer_utilisateur(email):
+    global utilisateurs
+    utilisateurs = [u for u in utilisateurs if u["email"] != email]
+    return utilisateurs
